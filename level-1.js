e3b355b6d8d08775dc6057cd83bb1d7ceb9cf252
tomerikfore <71603131+tomerikfore@users.noreply.github.com>
2020-10-04 23:56:33 +0200

    Create level-1.js

---
+++ level-1.js
@@ -0,0 +1,77 @@
+// question 1
+
+var gender = "male";
+
+// question 2
+
+var person = {
+    name: "John",
+    age: "30"
+{;
+
+// question 3
+
+var outOfStock = true;
+
+if(outOfStock === true) {
+  console.log("Out of stock")
+}
+else {
+  console.log("In stock")
+}
+
+// question 4
+
+var numbers = [10, 20, 30, 40, 50];
+
+for (var i = 0; i < numbers.length; i++){
+    console.log(numbers);
+}
+
+// question 5
+
+for(var count = 15; count <= 25; count++) {
+    console.log(count);
+}
+
+// question 6
+
+// question 7
+
+var newArray = [
+    {
+        string: "blabla",
+        number: 5,
+        boolean: true,
+    }
+    
+    {
+        string: "bleble",
+        number: 6,
+        boolean: false,
+    }
+];
+
+for(var i = 0; i < newArray.length; i++) {
+
+    console.log(catArray[i].name);
+
+    console.log(catArray[i].colour);
+
+    console.log(catArray[i].age);
+}
+
+// question 8
+
+function whatIDontLike(theUnlikable) {
+    console.log("I don't like");
+}
+
+function logToConsole(theUnlikable) {
+    console.log(theUnlikable);
+}
+
+logToConsole("spider");
+
+// question 9
+
